f2d37e9e46783fb1629224c633860097f13ab5c9
DanieleDAmbrosi <daniele.dambrosi04@gmail.com>
2022-12-14 11:11:49 +0100

    Project 1.0

---
+++ casino/src/main/java/com/casino/Server.java
@@ -0,0 +1,13 @@
+package casino.src.main.java.com.casino;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ casino/src/test/java/com/casino/AppTest.java
@@ -0,0 +1,20 @@
+package casino.src.test.java.com.casino;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
