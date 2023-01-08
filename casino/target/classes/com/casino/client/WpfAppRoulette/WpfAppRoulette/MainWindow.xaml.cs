8f839dc223eb78e38a072105cba3ed3ad4a26732
DanieleDAmbrosi <daniele.dambrosi04@gmail.com>
2023-01-08 21:12:41 +0100

    modifiche

---
+++ casino/target/classes/com/casino/client/WpfAppRoulette/WpfAppRoulette/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Threading;
+
+namespace WpfAppRoulette
+{
+   
+    public partial class MainWindow : Window
+    {
+        Storyboard s;
+        public MainWindow()
+        {
+            InitializeComponent();
+            s = (Storyboard)TryFindResource("spin");
+            
+        }
+
+        private void SpinButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            s.RepeatBehavior = new RepeatBehavior(3);
+            Thread t1 = new Thread(() => {
+                while (s.GetCurrentIteration() <= 3)
+                    s.SetSpeedRatio(s.GetCurrentGlobalSpeed());
+            });
+            s.Begin();
+            t1.Start();
+            
+        }
+    }
+}
