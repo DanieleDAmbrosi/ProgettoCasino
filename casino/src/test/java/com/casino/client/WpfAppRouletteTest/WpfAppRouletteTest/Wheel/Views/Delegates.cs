298ef996737d9cd06eeedc9ebb8a39bfdf171873
DanieleDAmbrosi <daniele.dambrosi04@gmail.com>
2023-01-11 10:43:30 +0100

    deleting wpf client

--- casino/src/test/java/com/casino/client/WpfAppRouletteTest/WpfAppRouletteTest/Wheel/Views/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿using RouletteSimulator.Core.Models.WheelModels;
-
-namespace Wheel.Views
-{
-    public delegate void WheelSpinning(bool wheelSpinning);
-    public delegate void BallTossed(bool ballTossed);
-    public delegate void WinningNumber(Pocket winningNumber);
-}
