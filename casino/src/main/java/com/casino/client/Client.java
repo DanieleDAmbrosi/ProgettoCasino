f426184014986186f01f63e9cdff6bad75c72953
DanieleDAmbrosi <daniele.dambrosi04@gmail.com>
2023-01-11 10:56:11 +0100

    gitignore update

--- casino/src/main/java/com/casino/client/Client.java
+++ casino/src/main/java/com/casino/client/Client.java
@@ -5,7 +5,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import com.casino.comm.messages.*;
