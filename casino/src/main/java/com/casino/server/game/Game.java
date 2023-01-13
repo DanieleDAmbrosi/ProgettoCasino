730a35949fd993aaa0a962c8479f430ab81df0f0
DanieleDAmbrosi <daniele.dambrosi04@gmail.com>
2023-01-13 18:07:36 +0100

    some changes

--- casino/src/main/java/com/casino/server/game/Game.java
+++ casino/src/main/java/com/casino/server/game/Game.java
@@ -1,6 +1,6 @@
 package com.casino.server.game;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.casino.comm.messages.ResetConnectionMessage;
 import com.casino.comm.player.PlayerState;
@@ -8,23 +8,23 @@ import com.casino.server.ClientConnectionHandler;
 
 public class Game {
 
-    private ArrayList<Player> players = new ArrayList<>();
+    //private ArrayList<Player> players = new ArrayList<>();
+    private HashMap<String, Player> players = new HashMap<>();
 
     public Game(){
 
     }
 
-    public Boolean addPlayer(ClientConnectionHandler client, String name){
-        return players.add(new Player(client, new PlayerState(), name));
+    public Boolean addPlayer(ClientConnectionHandler client, String name, String id){
+        Player player = new Player(client, new PlayerState(), name);
+        return players.put(id, player) != null;
     }
 
-    public void removePlayer(int id, ResetConnectionMessage resetConnectionMessage) {
-        Player player = null;
-        if((player = getPlayer(id)) != null) players.remove(player);
-        System.out.println("Player " + player.toString() + " left the lobby");
+    public void removePlayer(String id, ResetConnectionMessage resetConnectionMessage) {
+        System.out.println("Player " + players.remove(id).toString() + " left the lobby");
     }
 
-    private Player getPlayer(int id){
+    public Player getPlayer(String id){
         return players.get(id);
     }
 }
