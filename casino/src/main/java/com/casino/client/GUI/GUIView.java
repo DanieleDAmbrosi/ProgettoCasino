cc0de558fdb924e801ff89d5977ca7bf84950eec
matteogiud <matteogiud@icloud.com>
2023-01-16 21:33:51 +0100

    Create GUIView.java

---
+++ casino/src/main/java/com/casino/client/GUI/GUIView.java
@@ -0,0 +1,53 @@
+package com.casino.client.GUI;
+
+import com.casino.client.SendMessageToServer;
+import com.casino.client.View;
+import com.casino.comm.player.PlayerState;
+//import javafx.fxml.Initializable;
+
+
+public class GUIView implements  View{
+
+    @Override
+    public void setSendMessageToServer(SendMessageToServer sendMessageToServer) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resetConnection() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void doABet(PlayerState playerState, int seconds) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void welcome() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void showResults(int winnerNumber) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void closeBets() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
